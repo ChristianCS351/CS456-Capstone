5df79f2cb4ac45ce47e7498128547459f4d5c3f1
Matt Peters <petersm25@mycu.concord.edu>
2026-03-23 11:59:09 -0400

    Update login.php
    Fixed Logo and Deleted Footer

--- login.php
+++ login.php
@@ -106,7 +106,7 @@ if (isset($_POST['register'])) {
         <div class="nav-container">
             <div class="nav-left">
                 <a href="index.php" class="nav-brand">
-                    <i class="fa-solid fa-plane-departure"></i> Pantry Pilot
+                    <img src="PantryPilotlogo2.png" style="height: 50px;">
                 </a>
                 <a href="index.php">Home</a>
                 <a href="tracking.php">Pantry</a>
@@ -201,15 +201,5 @@ if (isset($_POST['register'])) {
             </div>
         </div>
     </main>
-
-    <!-- Footer -->
-    <footer>
-        <div class="footer-content">
-            <div class="footer-logo">
-                <i class="fa-solid fa-plane-departure"></i> Pantry Pilot
-            </div>
-            <p>&copy; 2026 Pantry Pilot. All rights reserved.</p>
-        </div>
-    </footer>
 </body>
 </html>
